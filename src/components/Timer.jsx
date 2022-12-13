cc790a6c5a8d4bdff3d9af5f22dcd630a057bba5
Scott Gibson <scottchgibson@gmail.com>
2022-12-12 18:10:29 -0800

    Timer responds to pressing spacebar to start and stop.

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -9,6 +9,18 @@ const Timer = (props) => {
     const [startTime, setStartTime] = useState(0);
     const [endTime, setEndTime] = useState(0);
 
+    /* Timer starts on space up, so the user can hold spacebar as long as
+    the like before starting the solve. And it stops on space down, so that
+    there is no lag time between hitting space and the timer stopping.
+
+    Unfortunately, when you hit space down, it stops the timer, and when you let
+    go of space, the event listener sees that and says, "okay, we're going again!"
+
+    To fix this, flag will be set to true when the timer is running, and will need
+    to be reset once before the timer will be set off by the spacebar.
+    */
+    const [flag, setFlag] = useState(false);
+
     const timer_interrupt = 53;
     // timer interrupt to update the timer on screen if running
     useEffect(() => {
@@ -20,6 +32,32 @@ const Timer = (props) => {
 
         return () => clearInterval(timer.current);
     }, [running]);
+
+    // add event listener for keydown and keyup
+    useEffect(() => {
+        window.addEventListener('keyup', handleKeyUp);
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keyup', handleKeyUp);
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+    })
+
+    const handleKeyUp = (event) => {
+        if (!flag && !running && event.code === 'Space') {
+            setFlag(true);
+            startTimer();
+        } else if (flag && event.code === 'Space') {
+            setFlag(false);
+        }
+    }
+
+    const handleKeyDown = (event) => {
+        if (running && event.code === 'Space') {
+            stopTimer();
+        }
+    }
 
     // on setTotal, return that time to the parent component. if total is 0,
     // assume this to be a false start and don't return anything.
@@ -46,12 +84,6 @@ const Timer = (props) => {
                 <h1 className='title is-1 bulma-unselectable-mixin'>
                     {running ? formatTime(endTime - startTime) : formatTime(total)}
                 </h1>
-            </div>
-            <div className='column'>
-                <button className='button is-primary'
-                    onClick={running ? stopTimer : startTimer}>
-                    Start/Stop
-                </button>
             </div>
         </div>
     );
