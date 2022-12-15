4fd771e01c0075200b1f316f6ad888d22a016960
Scott Gibson <scottchgibson@gmail.com>
2022-12-15 13:48:43 -0800

    Cube Scrambler alg made and lightly tested.

--- src/utils/CubetimerUtils.js
+++ src/utils/CubetimerUtils.js
@@ -10,5 +10,46 @@ export const formatTime = (time) => {
 }
 
 export const getScramble = () => {
-    return 'TODO: Scrambling alg';
+    const getRandomInt = (max) => {
+        return Math.floor(Math.random() * max)
+    }
+    const letters = ['U', 'D', 'L', 'R', 'F', 'B'];
+    const mods = ['', '\'', '2'];
+
+    let moves = [];
+    let prev_1 = -1;
+    let prev_2 = -1;
+    for (let i = 0; i < 20; i++) {
+        let cur = getRandomInt(6);
+        if (cur === prev_1) {
+            i -= 1;
+            continue;
+        }
+        if (i === 0) {
+            // create move
+            let letter = letters[cur];
+            let mod = mods[getRandomInt(mods.length)];
+            moves.push(letter + mod);
+
+            prev_1 = cur;
+            prev_2 = cur;
+        } else {
+            if (Math.floor(prev_1 / 2) === Math.floor(prev_2 / 2) && cur === prev_2) {
+                i -= 1;
+                continue;
+            }
+
+            // create move
+            let letter = letters[cur];
+            let mod = mods[getRandomInt(mods.length)];
+            moves.push(letter + mod);
+
+            prev_2 = prev_1;
+            prev_1 = cur;
+        }
+    }
+
+    let scramble = '';
+    moves.forEach((move) => scramble = scramble.concat(move, ' '));
+    return scramble;
 }
